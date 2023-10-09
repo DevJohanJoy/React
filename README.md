95ef23c6f090fc7bcaae44a31605ca36e24de60c
DevJohanJoy <145780189+DevJohanJoy@users.noreply.github.com>
2023-10-09 11:18:21 -0400

    On branch master Changes to be committed: 	new file:   .prettierrc 	new file:   LICENSE 	modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.tsx 	modified:   src/index.tsx

--- README.md
+++ README.md
@@ -1,46 +1,53 @@
-# Getting Started with Create React App
+# Basic TODO App with REACT TYPESCRIPT
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a simple Todo application built using React and TypeScript. It serves as a beginner-friendly project to help you learn the fundamentals of building a web application with these technologies.
 
-## Available Scripts
+A tutorial to help create you an app like this is readable on [dev.to](https://dev.to/devjohanjoy/basic-todo-app-with-react-typescript-43po).
 
-In the project directory, you can run:
+## Features
 
-### `npm start`
+-   Add new tasks to your todo list.
+-   Mark tasks as complete or incomplete.
+-   Remove tasks from the list.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## Installation
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+Before you start, make sure you have [Node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/) installed on your machine.
 
-### `npm test`
+1. Clone the repository:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+```bash
+  git clone <repository-url>
+```
 
-### `npm run build`
+2. Navigate to the project directory:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+```bash
+   cd basic-todo-app-react-ts
+```
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+3. Install the dependencies:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+```bash
+   npm install
+```
 
-### `npm run eject`
+4. Start the development server:
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+```bash
+   npm start
+```
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+5. Open your browser and visit [http://localhost:3000](http://localhost:3000) to see the app in action.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+## Technologies Used
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+-   React
+-   TypeScript
+-   CSS and Bootstrap (Styling)
 
-## Learn More
+## License
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+This project is licensed under the MIT License - see the [LICENSE](./LICENSE) file for details.
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+Happy coding!
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+interface Todo {
+    value: string;
+    completed: boolean;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const inputRef = React.useRef<HTMLInputElement>(null);
+    const [todosList, setTodosList] = React.useState<Todo[]>([]);
+
+    function addTodoItem() {
+        if (inputRef.current != null) {
+            if (
+                inputRef.current.value != null &&
+                inputRef.current.value.trim() != ''
+            ) {
+                let _todosList: Todo[] = [
+                    ...todosList,
+                    {
+                        value: inputRef.current.value,
+                        completed: false,
+                    },
+                ];
+
+                setTodosList(_todosList);
+            }
+        }
+    }
+
+    function toggleCompleteTodoItem(index: number) {
+        if (Number.isInteger(index) && index >= 0 && index < todosList.length) {
+            let _todosList: Todo[] = [...todosList];
+            _todosList[index].completed = !_todosList[index].completed;
+            setTodosList(_todosList);
+        }
+    }
+
+    function removeTodoItem(index: number) {
+        if (Number.isInteger(index) && index >= 0 && index < todosList.length) {
+            let _todosList: Todo[] = [...todosList];
+            _todosList.splice(index, 1);
+            setTodosList(_todosList);
+        }
+    }
+
+    return (
+        <div className='vw-100 vh-100 d-flex flex-column align-items-stretch p-4 gap-4'>
+            <div className='flex-grow-1 d-flex flex-column align-items-stretch gap-2 overflow-y-auto pe-2'>
+                {todosList.map((todo, index) => (
+                    <TodoItem
+                        todo={todo}
+                        toggleComplete={() => toggleCompleteTodoItem(index)}
+                        remove={() => removeTodoItem(index)}
+                    />
+                ))}
+            </div>
+            <div className='d-flex flex-row gap-2'>
+                <input
+                    ref={inputRef}
+                    type='text'
+                    className='flex-grow-1 form-control form-control-lg'
+                    placeholder='Add a task here'
+                />
+                <button
+                    type='button'
+                    className='btn btn-lg btn-primary'
+                    onClick={addTodoItem}
+                >
+                    Add
+                </button>
+            </div>
+        </div>
+    );
+}
+
+function TodoItem({
+    todo,
+    toggleComplete,
+    remove,
+}: {
+    todo: Todo;
+    toggleComplete: () => void;
+    remove: () => void;
+}) {
+    return (
+        <div className='card'>
+            <div className='card-body d-flex flex-row align-items-center gap-2'>
+                <div
+                    className={`card-text flex-grow-1 ${
+                        todo.completed ? 'text-decoration-line-through' : ''
+                    }`}
+                >
+                    {todo.value}
+                </div>
+                <button className='btn btn-primary' onClick={toggleComplete}>
+                    {todo.completed ? 'Uncomplete' : 'Complete'}
+                </button>
+                <button className='btn btn-danger' onClick={remove}>
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
